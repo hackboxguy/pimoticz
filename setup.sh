da7880df4ec4bf7fd04a185c9baa86e962af8d6e
Albert David <albert.david@gmail.com>
2020-06-21 00:06:21 +0200

    pimoticz: removed commented lines from setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-#./setup.sh -n 1 
+#./setup.sh -h hostname
 USAGE="usage:$0 -h <hostname> "
-#SLAVE_NUM="none"
 HOST_HAME="pimoticz"
 while getopts h: f
 do
@@ -11,31 +10,11 @@ do
 	esac
 done
 
-#if [ $# -lt 2  ]; then
-#	echo $USAGE
-#	exit 1
-#fi
 
 if [ $(id -u) -ne 0 ]; then
 	echo "Please run setup as root ==> sudo ./setup.sh -h my-optional-hostname"
 	exit
 fi
-
-#case $SLAVE_NUM in
-#   ''|*[!0-9]*)
-#		echo "Error: $SLAVE_NUM is not a number" && exit 1 ;;
-#    *) 
-#		NUM=$(printf "%04d" $SLAVE_NUM) ;;
-#esac
-
-#printf "Installing auto-startup-player ......................... "
-#rm -rf media-mux-autoplay.sh >/dev/null #remove any existing solft-link
-#if [ $NUM = "0001" ]; then
-#	ln -s media-mux-autoplay-master.sh media-mux-autoplay.sh
-#else
-#	ln -s media-mux-autoplay-slave.sh media-mux-autoplay.sh
-#fi
-#test 0 -eq $? && echo "[OK]" || echo "[FAIL]"
 
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 DEBIAN_FRONTEND=noninteractive apt-get update --fix-missing
@@ -69,19 +48,17 @@ systemctl enable ssh 1>/dev/null 2>/dev/null
 systemctl start ssh 1>/dev/null 2>/dev/null
 test 0 -eq $? && echo "[OK]" || echo "[FAIL]"
 
+
 printf "Installing zigbee2mqtt.................................. "
 git clone --quiet https://github.com/Koenkk/zigbee2mqtt.git /opt/zigbee2mqtt > /dev/null
 chown -R pi:pi /opt/zigbee2mqtt
 cp configuration.yaml /opt/zigbee2mqtt/data/
 cp zigbee2mqtt.service /etc/systemd/system/
-#pushd .
-#cd /opt/zigbee2mqtt
-#npm ci
-#popd
 npm ci --prefix /opt/zigbee2mqtt > "/dev/null" 2>&1
 systemctl --quiet enable zigbee2mqtt.service
 systemctl --quiet start zigbee2mqtt
 test 0 -eq $? && echo "[OK]" || echo "[FAIL]"
+
 
 printf "Installing domoticz..................................... "
 curl -L https://install.domoticz.com | bash
@@ -89,29 +66,12 @@ test 0 -eq $? && echo "[OK]" || echo "[FAIL]"
 
 
 printf "Installing domoticz-plugins............................. "
-pushd .
-cd /home/pi/domoticz/plugins/
-git clone --quiet https://github.com/stas-demydiuk/domoticz-plugins-manager.git plugins-manager > /dev/null
-git clone --quiet https://github.com/stas-demydiuk/domoticz-zigbee2mqtt-plugin.git zigbee2mqtt > /dev/null
+git clone --quiet https://github.com/stas-demydiuk/domoticz-plugins-manager.git /home/pi/domoticz/plugins/plugins-manager > /dev/null
+git clone --quiet https://github.com/stas-demydiuk/domoticz-zigbee2mqtt-plugin.git /home/pi/domoticz/plugins/zigbee2mqtt > /dev/null
 test 0 -eq $? && echo "[OK]" || echo "[FAIL]"
-popd
 
 systemctl --quiet enable domoticz.service
 systemctl --quiet restart domoticz.service
-
-#printf "Forcing audio output to analog-out ..................... "
-#amixer cset numid=3 1 > /dev/null #0-automatic 1-analog 2-hdmi
-#test 0 -eq $? && echo "[OK]" || echo "[FAIL]"
-
-#printf "Compiling media-mux-controller-server................... "
-#gcc media-mux-controller.c -o media-mux-controller 1>/dev/null 2>/dev/null
-#test 0 -eq $? && echo "[OK]" || echo "[FAIL]"
-
-#for master, enable dhcp server
-#if [ $NUM = "0001" ]; then
-#	sudo apt-get -y install isc-dhcp-server
-#	cp dhcpd.conf /etc/dhcp/
-#fi
 
 sync
 
