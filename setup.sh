f453586776fe1691cd0bbfd2c75e4927015b4607
Albert David <albert.david@gmail.com>
2020-06-29 16:50:15 +0200

    pimoticz: updated setup.sh to include jq

--- setup.sh
+++ setup.sh
@@ -21,7 +21,7 @@ DEBIAN_FRONTEND=noninteractive apt-get update --fix-missing
 
 #install dependencies
 printf "Installing dependencies ................................ "
-DEBIAN_FRONTEND=noninteractive apt-get install -qq avahi-daemon avahi-discover libnss-mdns avahi-utils nodejs mosquitto mosquitto-clients < /dev/null > /dev/null
+DEBIAN_FRONTEND=noninteractive apt-get install -qq avahi-daemon avahi-discover libnss-mdns avahi-utils nodejs mosquitto mosquitto-clients jq < /dev/null > /dev/null
 test 0 -eq $? && echo "[OK]" || echo "[FAIL]"
 
 
